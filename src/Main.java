54d3a997c18faaa14fcc45160b3039d6a3f76b77
Vlad-Gabriel Sica <sicavgabriel@yahoo.com>
2025-07-21 21:52:08 +0200

    Tema Introducere in Java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+
+    public class Main {
+        public static void main(String[] args) {
+
+            System.out.println("Hello");
+            System.out.println("Vlad");
+
+            System.out.println(3+5);
+
+            System.out.println(22/3.0);
+
+            System.out.println(-5+8*6);
+
+            System.out.println((55+9) % 9 );
+
+            System.out.println(20 + -3*5 / 8);
+
+            System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+        }
+    }
